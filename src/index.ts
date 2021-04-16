ffcce2c0d98aaec6a5d4632a6cf6d2b97ae8cc0b
lucas.dittrich <ld.dittrich@gmail.com>
2021-04-16 19:20:37 -0300

    refactor: export theme
    Signed-off-by: lucas.dittrich <ld.dittrich@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export { default as ThemeProvider } from './theme/ThemeProvider'
+
 export { default as Button } from './core/Button'
+export { default as Text } from './core/Text'
 export { default as Title } from './core/Title'
