f461927fce1e5a8fcbbed18b50388aad554f75fe
dittrichlucas <ld.dittrich@gmail.com>
2021-08-12 19:47:46 -0300

    refactor: refactor texfield story
    Signed-off-by: dittrichlucas <ld.dittrich@gmail.com>

--- src/stories/TextField.stories.tsx
+++ src/stories/TextField.stories.tsx
@@ -2,19 +2,35 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import TextField from '../core/Form/TextField'
-import { TextFieldProps } from '../core/Form/TextField/style'
+import { StyledTextFieldProps } from '../core/Form/TextField/style'
 
 export default {
     title: 'Example/TextField',
     component: TextField,
+    argTypes: {
+        color: {
+            control: {
+                type: 'select',
+                options: ['default', 'primary', 'secondary']
+            }
+        },
+        variant: {
+            control: {
+                type: 'select',
+                options: ['outlined', 'text']
+            }
+        }
+    }
 } as Meta
 
-const Template: Story<TextFieldProps> = (args) => <TextField {...args} />
+const Template: Story<StyledTextFieldProps> = (args) => <TextField {...args} />
 
 export const Input = Template.bind({})
 Input.args = {
-    value: 'mail@mail.com',
+    value: '',
+    color: 'primary',
     type: 'text',
     onChange: () => {},
-    placeholder: 'Email'
+    placeholder: 'Email',
+    variant: 'outlined',
 }
