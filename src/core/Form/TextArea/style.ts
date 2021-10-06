71aab299e97d838c14be5757a14e5ec4a69beecb
dittrichlucas <ld.dittrich@gmail.com>
2021-10-05 21:38:02 -0300

    feat: add textarea component
    Signed-off-by: dittrichlucas <ld.dittrich@gmail.com>

---
+++ src/core/Form/TextArea/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import { StyledBoxProps } from '../TextField/style'
+
+export type TextAreaProps =
+    & StyledBoxProps
+    & {
+        id?: string
+        value?: string
+        required?: boolean
+        label: string
+        placeholder?: string
+        wrap?: string
+        cols?: number
+        rows?: number
+        onChange?: () => void
+    }
+
+export const StyledTextArea = styled('textarea')(({ theme }) => ({
+    fontFamily: theme.fonts.fontFamily,
+    borderRadius: '5px',
+    fontSize: theme.fontSizes[16],
+    background: 'none',
+    border: 'none',
+    outline: 'none'
+}))
